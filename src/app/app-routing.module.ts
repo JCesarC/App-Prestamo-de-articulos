2757f3be9563e4279fa20d5f5ae27c92de584f91
JCesarC <julio.jccp.1@gmail.com>
2021-10-02 22:55:09 -0500

    added routes and notfound page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [{ path: 'notFound', loadChildren: () => import('./pages/notfound/notfound.module').then(m => m.NotfoundModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'prestamos', loadChildren: () => import('./pages/prestamos/prestamos.module').then(m => m.PrestamosModule) }, { path: 'articulos', loadChildren: () => import('./pages/articulos/articulos.module').then(m => m.ArticulosModule) }, { path: 'usuarios', loadChildren: () => import('./pages/usuarios/usuarios.module').then(m => m.UsuariosModule) }];
+const routes: Routes = [
+  {
+    path: 'notFound',
+    loadChildren: () =>
+      import('./pages/notfound/notfound.module').then((m) => m.NotfoundModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'prestamos',
+    loadChildren: () =>
+      import('./pages/prestamos/prestamos.module').then(
+        (m) => m.PrestamosModule
+      ),
+  },
+  {
+    path: 'articulos',
+    loadChildren: () =>
+      import('./pages/articulos/articulos.module').then(
+        (m) => m.ArticulosModule
+      ),
+  },
+  {
+    path: 'usuarios',
+    loadChildren: () =>
+      import('./pages/usuarios/usuarios.module').then((m) => m.UsuariosModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'notFound' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
